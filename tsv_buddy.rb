2131dfdcdd3fedb82c124de6c8eef36afd6d24ca
irina <syrok.BU@yandex.ru>
2024-09-26 14:42:23 +0800

    All tests pass; rubocop/flog resolved

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -2,17 +2,26 @@
 
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
+  @data = []
+
   # Converts a String with TSV data into internal data structure @data
   # arguments: tsv - a String in TSV format
   # returns: nothing
   def take_tsv(tsv)
-
+    lines = tsv.split("\n")
+    keys = lines.shift.split
+    @data = lines.map do |line|
+      values = line.split("\t").map(&:chomp)
+      keys.zip(values).to_h
+    end
   end
 
   # Converts @data into tsv string
   # arguments: none
   # returns: String in TSV format
   def to_tsv
-
+    headers = @data[0].keys.join("\t")
+    rows = @data.map { |row| row.values.join("\t") }
+    "#{([headers] + rows).join("\n")}\n"
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,3 +1,22 @@
 # frozen_string_literal: true
 
+require 'yaml'
+
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  @data = []
+
+  # Takes a yaml string and converts it into internal data structure @data
+  # arguments: yaml - a String in YAML format
+  # returns: nothing
+  def take_yaml(yaml)
+    @data = YAML.safe_load(yaml)
+  end
+
+  # Converts @data into yaml string
+  # arguments: none
+  # returns: String in YAML format
+  def to_yaml
+    @data.to_yaml
+  end
+end
